26a5ee4b729ae8b776cb08fff6b65807c7a502dd
ArkadiuszBramowski <135068702+ArkadiuszBramowski@users.noreply.github.com>
2023-09-22 23:35:29 +0200

    Tuple training

--- Module 2/Task 019.py
+++ Module 2/Task 019.py
@@ -1,4 +1,20 @@
 # Below enter lowercase letters only those words from the given sentence
 # that start and end with the same letter.
 
+sentence = input('Give a sentence: ')
+
+for word in sentence.split(' '):
+    # if word[0].lower() == word[-1].lower():
+    if word.lower().startswith(word[-1].lower()):
+        print(word)
+
 # Below display all sentences with vowels removed.
+
+new_sentence = ''
+vowels = 'aeiouy'
+
+for char in sentence:
+    if char.lower() not in vowels:
+        new_sentence += char
+
+print(new_sentence)
